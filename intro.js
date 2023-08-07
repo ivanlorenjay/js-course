9a373bd45b00ea56a8045fc6b05366824232f684
Ivan Lorenzo <105089712+ivanlorenjay@users.noreply.github.com>
2023-08-06 19:03:36 -0600

    intro.js first commit

---
+++ intro.js
@@ -0,0 +1,66 @@
+// Tipos de datos 
+
+// En js tenemos los tipos de datos primitivos que son los que interactúan directamente con las operaciones 
+// son 7 tipos y estos son: 
+
+// - number -> 4 / 4.5
+// - string -> "Hola mundo"
+// - boolean -> true / false
+// - null
+// - undefined
+// - symbol
+// - bigint
+
+// El operador de más (+) con los números se comporta de una forma y con las cadenas se comporta
+// de otra forma. Son operadores distintos: suma con los números y concatenación con las cadenas
+
+// Operadores de compración
+
+(5 > 3)
+(5 >= 5)
+(5 === 6)
+(5 != 4)
+
+
+// Operadores lógicos
+
+true && true // true
+true && false // false 
+
+true || true // true 
+true || false // true 
+false || true // true
+false || false // false 
+
+!true // false 
+!false // true
+
+// Variables 
+
+let numero = 4;
+numero + 2; 
+console.log(numero); 
+
+numero = 10; 
+otroNumero = numero; 
+console.log(otroNumero);
+
+let texto = "cadena"; 
+
+// Constantes 
+// No cambia su valor y siempre se inicializan con alguno 
+
+const IS_DISABLED = true
+
+// Null & undefined 
+// una variable solo puede ser nula asignándole el valor de null, que no tiene valor
+
+let money = null; 
+
+// undefined quiere decir que no ha sido definido y no se sabe de qué tipo de dato se trata, 
+// o que se asignó a drede el valor de undefined
+
+let capacidad; // undefined
+let capacidad2 = undefined; // undefined
+
+
